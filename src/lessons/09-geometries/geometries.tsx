35bf27f6ecfe619da1e4021f83103e51f10c5e26
sebastienlorber <lorber.sebastien@gmail.com>
2021-12-12 21:39:51 +0100

    complete geometries lesson + upgrade to R3f 8 / React 18

--- src/lessons/09-geometries/geometries.tsx
+++ src/lessons/09-geometries/geometries.tsx
@@ -1,15 +1,67 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import * as THREE from "three";
 import useConstant from "use-constant";
 
+function Triangle() {
+  const position = useMemo(
+    () =>
+      new Float32Array([
+        // First vertex
+        0, 0, 0,
+        // Second vertex
+        0, 1, 0,
+        // Third vertex
+        1, 0, 0,
+      ]),
+    [],
+  );
+
+  return (
+    <bufferGeometry>
+      <bufferAttribute attach="attributes-position" args={[position, 3]} />
+    </bufferGeometry>
+  );
+}
+
+function CustomGeometry() {
+  const count = 20;
+  const size = 1;
+
+  const position = useMemo(() => {
+    const values = [...Array(count * 3 * 3).keys()].flatMap((index) => {
+      return (Math.random() - 0.5) * size;
+    });
+    return new Float32Array(values);
+  }, [count, size]);
+
+  return (
+    <bufferGeometry>
+      <bufferAttribute attach="attributes-position" args={[position, 3]} />
+    </bufferGeometry>
+  );
+}
+
 function Scene() {
   return (
     <>
       <mesh position={[0, 0, 0]}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshBasicMaterial color={"red"} />
+        <boxGeometry args={[1, 1, 1, 2, 2, 2]} />
+        <meshBasicMaterial color={"red"} wireframe />
+      </mesh>
+
+      <mesh position={[2, 0, 0]}>
+        <sphereGeometry args={[1, 10, 5]} />
+        <meshBasicMaterial color={"green"} wireframe />
+      </mesh>
+      <mesh position={[-2, 0, 0]}>
+        <Triangle />
+        <meshBasicMaterial color={"blue"} wireframe />
+      </mesh>
+      <mesh position={[0, 2, 0]}>
+        <CustomGeometry />
+        <meshBasicMaterial color={"lime"} wireframe />
       </mesh>
       <axesHelper />
     </>
@@ -20,6 +72,7 @@ export default function Geometries() {
   return (
     <Canvas>
       <Scene />
+      <OrbitControls />
     </Canvas>
   );
 }
