72251ac05ced96aae65baf9f4677f8963712f106
sebastienlorber <lorber.sebastien@gmail.com>
2021-12-19 20:00:59 +0100

    complete texture

--- src/lessons/11-textures/textures.tsx
+++ src/lessons/11-textures/textures.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import * as THREE from "three";
 import useConstant from "use-constant";
@@ -7,12 +7,57 @@ import gsap from "gsap";
 
 import { Leva, folder, button, useControls } from "leva";
 
+import { TextureLoader } from "three/src/loaders/TextureLoader";
+
 function Scene() {
+  const [
+    colorTexture,
+    alphaTexture,
+    heightTexture,
+    normalTexture,
+    ambientOcclusionTexture,
+    metalnessTexture,
+    roughnessTexture,
+  ] = useLoader(TextureLoader, [
+    new URL("./textures/door/color.jpg", import.meta.url).href,
+    // new URL("./textures/checkerboard-1024x1024.png", import.meta.url).href,
+    // new URL("./textures/checkerboard-8x8.png", import.meta.url).href,
+    // new URL("./textures/minecraft.png", import.meta.url).href,
+
+    new URL("./textures/door/alpha.jpg", import.meta.url).href,
+    new URL("./textures/door/height.jpg", import.meta.url).href,
+    new URL("./textures/door/normal.jpg", import.meta.url).href,
+    new URL("./textures/door/ambientOcclusion.jpg", import.meta.url).href,
+    new URL("./textures/door/metalness.jpg", import.meta.url).href,
+    new URL("./textures/door/roughness.jpg", import.meta.url).href,
+  ]);
+
+  /*colorTexture.repeat.x = 2;
+  colorTexture.repeat.y = 3;
+  colorTexture.wrapS = THREE.RepeatWrapping;
+  colorTexture.wrapT = THREE.MirroredRepeatWrapping;
+  colorTexture.offset.x = 0.5;
+  colorTexture.offset.y = 0.5;
+  */
+
+  /*
+  colorTexture.center.x = 0.5;
+  colorTexture.center.y = 0.5;
+  colorTexture.rotation = Math.PI / 4;
+     */
+
+  // colorTexture.minFilter = THREE.NearestFilter;
+  // colorTexture.magFilter = THREE.NearestFilter;
+  // colorTexture.generateMipmaps = false;
+
   return (
     <>
       <mesh position={[0, 0, 0]}>
         <boxGeometry args={[1, 1, 1]} />
-        <meshBasicMaterial color={"red"} wireframe={true} />
+        {/*<sphereGeometry args={[1, 32, 32]} />
+        <coneGeometry args={[1, 1, 32]} />
+        <torusGeometry args={[1, 0.35, 32, 100]} />*/}
+        <meshBasicMaterial map={colorTexture} />
       </mesh>
       <axesHelper />
     </>
