a235dc48e2d672ecbc4f31ed3a4895a69fffe81a
sebastienlorber <lorber.sebastien@gmail.com>
2022-01-08 19:04:42 +0100

    complete 3d text

--- src/lessons/13-3d-text/3d-text.tsx
+++ src/lessons/13-3d-text/3d-text.tsx
@@ -1,23 +1,35 @@
-import React, { useLayoutEffect, useMemo, useRef } from "react";
+import React, {
+  ComponentProps,
+  ComponentType,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+} from "react";
 import { Canvas, useLoader, extend } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import * as THREE from "three";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
+import type { Vector3 } from "@react-three/fiber";
 
 extend({ TextGeometry });
 
 function Scene() {
-  const fontUrl = new URL(
-    "./fonts/helvetiker_regular.typeface.json",
-    import.meta.url,
-  ).href;
+  const font = useLoader(
+    FontLoader,
+    new URL("./fonts/helvetiker_regular.typeface.json", import.meta.url).href,
+  );
 
-  const font = useLoader(FontLoader, fontUrl);
+  const matcapTexture = useLoader(
+    TextureLoader,
+    new URL("./textures/matcaps/1.png", import.meta.url).href,
+  );
 
   const config = useMemo(
     () => ({
       font,
-      size: 4,
+      size: 2,
       height: 1,
 
       curveSegments: 3,
@@ -30,7 +42,7 @@ function Scene() {
     [font],
   );
 
-  const text = "hello world";
+  const text = "Hello";
 
   const textRef = useRef<any>();
 
@@ -38,15 +50,58 @@ function Scene() {
     textRef.current.geometry.center();
   }, [text]);
 
+  const geometry = <torusGeometry args={[0.3, 0.2, 20, 45]} />;
+  //const material = <meshMatcapMaterial matcap={matcapTexture} />;
+  const material = <meshNormalMaterial />;
+
   return (
-    <mesh ref={textRef} position={[0, 0, 0]}>
-      <meshBasicMaterial wireframe={true} />
-      <textGeometry args={[text, config]} />
+    <>
+      <mesh ref={textRef} position={[0, 0, 0]}>
+        {material}
+        <textGeometry args={[text, config]} />
+      </mesh>
+      {[...Array(200).keys()].map((index) => {
+        return <Donut key={index} geometry={geometry} material={material} />;
+      })}
+    </>
+  );
+}
+
+function Donut({
+  material,
+  geometry,
+}: {
+  material: JSX.Element;
+  geometry: JSX.Element;
+}) {
+  const position: Vector3 = useMemo(
+    () => [
+      (Math.random() - 0.5) * 10,
+      (Math.random() - 0.5) * 10,
+      (Math.random() - 0.5) * 10,
+    ],
+    [],
+  );
+
+  const rotation: Vector3 = useMemo(
+    () => [Math.random() * Math.PI, Math.random() * Math.PI, 0],
+    [],
+  );
+
+  const scale: Vector3 = useMemo(() => {
+    const randomScale = Math.random() * 0.4;
+    return [randomScale, randomScale, randomScale];
+  }, []);
+
+  return (
+    <mesh position={position} rotation={rotation} scale={scale}>
+      {material}
+      {geometry}
     </mesh>
   );
 }
 
-export default function Webpack() {
+export default function ThreeDText() {
   return (
     <Canvas>
       <Scene />
